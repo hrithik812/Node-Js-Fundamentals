c08e69c9944889d3d1f59e258b611ccb53ad373b
hrithik812 <hrithik08.rudra@gmail.com>
2022-10-23 10:56:17 +0600

    Lecture 4 updated

--- Lecture4.js
+++ Lecture4.js
@@ -1,1 +1,27 @@
-console.log("Heelo");
+//http module
+
+const http=require('http');
+const port=3000;
+
+const server=http.createServer((req,res)=>{
+        if(req.url=='/'){
+       res.write('Hello Programmers');
+       res.end();
+        }else if(req.url=='/about'){
+       res.write('How are you???');
+       res.end();
+       }else{
+           res.write('Not found')
+           res.end();
+       }
+});
+//listening on event
+// Internally when a req with port is requste then this happens internally
+// server.on('connection',()=>{
+//     console.log('Listening on port',port);
+
+// })
+
+server.listen(port);
+
+
